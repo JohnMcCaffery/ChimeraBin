c560e776a9825c3056599ff77260b6929aefce5f
John 780-1 <jm726@st-andrews.ac.uk>
2013-09-12 16:47:07 +0100

    Extended startup time after launching OS to 2m

--- Scripts/startup.bat
+++ Scripts/startup.bat
@@ -14,6 +14,6 @@ cd C:\Users\openvritualworlds\Desktop\Opensim-Timespan\
 start "OpenSim" /MAX OpenSim.exe
 cd C:\Users\openvritualworlds\Documents\John\Chimera\Bin\
 
-timeout 60
+timeout 120
 
 Scripts\launch.bat
