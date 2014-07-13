13a9ad1020313a3995f0140319ee3ff1831c4635
JohnExperimentMachine <jm726@st-andrews.ac.uk>
2014-07-13 15:11:54 +0100

    Tweaked MultiRunConfig script to copy config folders correctly.

--- Scripts/MultiRunConfig.sh
+++ Scripts/MultiRunConfig.sh
@@ -1,18 +1,20 @@
 echo Running $1 with $2 config $3 times
 
+echo Copying Configs/$2 to Configs/tmp
 cp -r Configs/$2 Configs/tmp
 
 for ((i = 1; i <= $3; i++)); 
 do
-	echo Repeat: $i
+	echo Repeat: $i of $3
 	Scripts/Repeat.sh $1
 
+	echo Replacing Configs/$2 with Configs/tmp/$2
 	rm -r Configs/$2
-	cp -r Configs/tmp Configs/$2 
+	cp -r Configs/tmp/$2 Configs
 done
 
-rm -r Configs/$2
-mv Configs/tmp Configs/$2 
+echo Removing Configs/tmp
+rm -r Configs/tmp
 
 if [ "$4" = "shutdown" ]
 then
